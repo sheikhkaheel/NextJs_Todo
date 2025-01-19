7aa920126a6cff85737b1a8fb67237e1232228ce
sheikhkaheel <sheikhkaheel@gmail.com>
2025-01-19 15:53:21 +0530

    a

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,22 @@ import Todos from "./components/list-of-todo";
 import AddTodo from "./components/add-todo";
 
 // Cache tasks on the server side
-const tasks = unstable_cache(async () => await getTasks(), ["task"], {
-  revalidate: 3600,
-  tags: ["task"],
-});
+// const tasks = unstable_cache(async () => await getTasks(), ["task"], {
+//   revalidate: 3600,
+//   tags: ["task"],
+// });
 
 export default async function Home() {
   // Fetch tasks using the cached server-side function
-  const allTasks = await tasks();
+  // const allTasks = await tasks();
 
   return (
     <div className="p-8 bg-neutral-700 h-screen w-full m-0">
-      <div className="mb-8">
+      {/* <div className="mb-8">
         <AddTodo />
       </div>
       <h1 className="mb-4 font-semibold text-3xl">List of Todo</h1>
-      <Todos allTasks={allTasks} />
+      <Todos allTasks={allTasks} /> */}
     </div>
   );
 }
