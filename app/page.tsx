34e07626a076b02e956fcc2800d1060233383b8c
sheikhkaheel <sheikhkaheel@gmail.com>
2025-01-19 16:49:09 +0530

    modified action.ts

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,13 @@ import { unstable_cache } from "next/cache";
 import Todos from "./components/list-of-todo";
 import AddTodo from "./components/add-todo";
 
-// const tasks = unstable_cache(async () => await getTasks(), ["task"], {
-//   revalidate: 3600,
-//   tags: ["task"],
-// });
+const tasks = unstable_cache(async () => await getTasks(), ["task"], {
+  revalidate: 3600,
+  tags: ["task"],
+});
 
 export default async function Home() {
-
-  // const allTasks = await tasks();
-  // const allTasks = await getTasks();
+  const allTasks = await tasks();
 
   return (
     <div className="p-8 bg-neutral-700 h-screen w-full m-0">
@@ -21,7 +19,7 @@ export default async function Home() {
         <AddTodo />
       </div>
       <h1 className="mb-4 font-semibold text-3xl">List of Todo</h1>
-      {/* <Todos allTasks={allTasks} /> */}
+      <Todos allTasks={allTasks} />
     </div>
   );
 }
