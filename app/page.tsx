8c0ddda0613290cb25401d74a37854b9e26e5a99
sheikhkaheel <sheikhkaheel@gmail.com>
2025-01-19 16:10:46 +0530

    abcd

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,20 @@ import Todos from "./components/list-of-todo";
 import AddTodo from "./components/add-todo";
 
 const tasks = unstable_cache(async () => await getTasks(), ["task"], {
-  // revalidate: 3600,
+  revalidate: 3600,
   tags: ["task"],
 });
 
-
-
 export default async function Home() {
-  
-  // const allTasks = await tasks();
-  const allTasks = await getTasks();
+  const allTasks = await tasks();
 
   return (
     <div className="p-8 bg-neutral-700 h-screen w-full m-0">
       <div className="mb-8">
-        <AddTodo />
+        {/* <AddTodo /> */}
       </div>
       <h1 className="mb-4 font-semibold text-3xl">List of Todo</h1>
-      <Todos allTasks={allTasks} />
+      {/* <Todos allTasks={allTasks} /> */}
     </div>
   );
 }
